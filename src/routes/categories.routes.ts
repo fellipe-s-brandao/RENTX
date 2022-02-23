55e1b8ae0d1ac26ac18cb7db292e6446896fb7ad
Fellipe Brandao <fellipesilverio31@hotmail.com>
2022-02-22 21:50:20 -0300

    criação de rotas e repositoy de categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,28 @@
-import { request, response, Router } from "express";
+import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
-
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    categories.push({
-        name,
-        description
-    });
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
 
+    if(categoryAlreadyExists){
+        return response.status(400).json({error: 'Category already exists!'})
+    }
+    
+    categoriesRepository.create({ name, description })
+    
     return response.status(201).send();
-})
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all);
+});
 
 export { categoriesRoutes };
