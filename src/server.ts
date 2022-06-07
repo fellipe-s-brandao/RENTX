af7f9aac9d12761742fa1aebfc8a189309d59326
Fellipe Brandão <fellipesilverio31@hotmail.com>
2022-06-07 13:29:13 -0300

    inserindo typeorm e configurando docker do postgres

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import express  from "express";
 import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
 
-import swaggerFile from "./swagger.json"
+import swaggerFile from "./swagger.json";
+
+import "./database";
 
 const app = express();
 
