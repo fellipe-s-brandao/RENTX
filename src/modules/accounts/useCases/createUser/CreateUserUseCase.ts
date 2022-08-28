660243f64562939999da9e8d74f4a7a93eceb251
Fellipe Brandao <fellipesilverio31@hotmail.com>
2022-08-28 20:47:25 -0300

    refact: removendo username

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -13,7 +13,6 @@ class CreateUserUseCase {
     async execute(data: ICreateUserDTO): Promise<void> {
         await this.userRepository.create({
             name: data.name,
-            username: data.username,
             email: data.email,
             driver_license: data.driver_license,
             password: data.password
