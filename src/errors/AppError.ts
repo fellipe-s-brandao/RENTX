d8d10755528a67e1bf93384e7995501b6cd06961
Fellipe Brandao <fellipesilverio31@hotmail.com>
2022-08-29 21:37:37 -0300

    feat: inicio de implemetação de middleware de autenticação e classe de tratamento de erros

---
+++ src/errors/AppError.ts
@@ -0,0 +1,5 @@
+export class AppError {
+    public readonly message: string;
+
+    public readonly statusCode: number;
+}
--- src/modules/accounts/repositories/implementations/UserRepository.ts
+++ src/modules/accounts/repositories/implementations/UserRepository.ts
@@ -23,8 +23,12 @@ class UsersRepository implements IUsersRepository {
     
     async findByEmail(email: string): Promise<User> {
         const user = await this.repository.findOne({ email });
+        return user;
+    }
 
-        return user
+    async findById(id: string): Promise<User> {
+        const user = await this.repository.findOne(id);
+        return user;
     }
 }
 
