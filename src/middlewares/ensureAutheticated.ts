9b9b688bb0c9ad80a77a47747aa7143649b04ca2
Fellipe Brandao <fellipesilverio31@hotmail.com>
2022-08-29 21:53:04 -0300

    feat: tratativa de erros

--- src/middlewares/ensureAutheticated.ts
+++ src/middlewares/ensureAutheticated.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
+import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../modules/accounts/repositories/implementations/UserRepository";
 
 interface IPayload {
@@ -11,7 +12,7 @@ export async function ensureAuthenticated(request: Request,  response: Response,
     const authHeader = request.headers.authorization;
 
     if(! authHeader){
-        throw new Error("Token missing");
+         new AppError("Token missing", 401);
     }
 
     const [, token] =authHeader.split(" ");
@@ -22,12 +23,12 @@ export async function ensureAuthenticated(request: Request,  response: Response,
         const user = usersRepository.findById(user_id);
 
         if(! user){
-            throw new Error("User does not exists");
+            throw new AppError("User does not exists", 401);
         }
 
         next();
 
     } catch (error) {
-        throw new Error("Invalid token!");
+        throw new AppError("Invalid token!", 401);
     }
 }
--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
+import { AppError } from "../../../../errors/AppError";
 
 interface IRequest {
     email: string,
@@ -27,13 +28,13 @@ class AuthenticateUserUseCase {
         const user = await this.usersRepository.findByEmail(email);
 
         if(!user) {
-            throw new Error("Email or password incorrect!");
+            throw new AppError("Email or password incorrect!");
         }
 
         const passwordMatch = await compare(password, user.password);
 
         if(!passwordMatch) {
-            throw new Error("Email or password incorrect!");
+            throw new AppError("Email or password incorrect!");
         }
 
         const token = sign({}, "as65sa4d1x5s5s4aa1152ss", {
